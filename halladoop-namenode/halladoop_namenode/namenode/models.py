3f350049b288d7b28eb9db225f9a9bb8e462e6e8
stevenulibarri <git.sulibarri@gmail.com>
2015-12-07 09:50:01 -0700

    changed name_node directory name to namenode

---
+++ halladoop-namenode/halladoop_namenode/namenode/models.py
@@ -0,0 +1,8 @@
+
+class Registration(object):
+
+    def __init__(self, nodeID, nodeIP, totalDiskSpaceMb, availableDiskSpaceMB):
+        self.nodeID = nodeID
+        self.nodeIP = nodeIP,
+        self.totalDiskSpaceMb = totalDiskSpaceMb,
+        self.availableDiskSpaceMB = availableDiskSpaceMB
