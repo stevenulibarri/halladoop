b62242bbaa78448dbfb62a3e2dd4e30372ae26ab
stevenulibarri <git.sulibarri@gmail.com>
2015-12-13 20:26:17 -0700

    updated write to return node idmip pairs

--- halladoop-namenode/halladoop_namenode/namenode/requesthandlers/requesthandlers.py
+++ halladoop-namenode/halladoop_namenode/namenode/requesthandlers/requesthandlers.py
@@ -95,7 +95,7 @@ def handle_finalize(finalize_request):
         vfs.add_block_entry(block_id, node_id)
 
 def handle_write(write_request):
-    nodes = nodemanager.get_nodes_for_write(config.REPLICATION_FACTOR)
+    nodes = node_manager.get_nodes_for_write(config.REPLICATION_FACTOR)
     return responsemodels.WriteResponse(nodes)
 
 def handle_read(file_path):
