2edbdd8e342f9848cbe5243e289e2ade9b6c1d48
David <borlandavid@gmail.com>
2015-12-14 17:47:30 -0700

    Worked on finalizing and fixin some bugs in the client.

--- halladoop-client/src/main/java/com/distributed_systems/halladoop/client/HalladoopClient.java
+++ halladoop-client/src/main/java/com/distributed_systems/halladoop/client/HalladoopClient.java
@@ -1,13 +1,13 @@
 package com.distributed_systems.halladoop.client;
 
-import com.distributed_systems.halladoop.dataNode.model.ReadWorker;
-import com.distributed_systems.halladoop.dataNode.model.WriteWorker;
-
 import java.io.File;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+
+import com.distributed_systems.halladoop.dataNode.model.ReadWorker;
+import com.distributed_systems.halladoop.dataNode.model.WriteWorker;
 
 /**
  * Created by devin on 12/3/15.
