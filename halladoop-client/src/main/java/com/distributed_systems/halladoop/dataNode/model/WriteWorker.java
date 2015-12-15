e1f79b317bff8d761850e198198ab4e6b8e55fde
Garrett Holbrook <garrett92895@yahoo.com>
2015-12-14 17:17:32 -0700

    Fixed client to have a separate file_path argument

--- halladoop-client/src/main/java/com/distributed_systems/halladoop/dataNode/model/WriteWorker.java
+++ halladoop-client/src/main/java/com/distributed_systems/halladoop/dataNode/model/WriteWorker.java
@@ -33,10 +33,12 @@ public class WriteWorker implements Runnable {
     private static final int DATA_NODE_PORT = 4567;
 
     private final File file;
+    private final String fileName;
     private final String host;
     private final int port;
 
-    public WriteWorker(File file, String host, int port) {
+    public WriteWorker(String fileName, File file, String host, int port) {
+        this.fileName = fileName;
         this.file = file;
         this.host = host;
         this.port = port;
@@ -56,7 +58,7 @@ public class WriteWorker implements Runnable {
             List<WriteData> blocks = createBlocks(file);
 
             String a  ="{" +
-                    "\"file_path\": \"/" + file.getName() + "\","
+                    "\"file_path\": \"/" + this.fileName + "\","
                     + "\"num_blocks\": \"" + blocks.size()
                     + "\"}";
             StringEntity entity = new StringEntity(a);
